b8ebf5f411a91d88e7b0dd5d57727932e094b92a
cater <cater168fs@163.com>
2019-05-26 09:46:02 +0800

    <update> | 增加备案号放置

--- _site/js/song.js
+++ _site/js/song.js
@@ -3,6 +3,8 @@ var songList = [
 {"name":"愿得一人心","songer":"cater","edtion":"张行亮","duration":"3:53","url":"http://upuwmp3.changba.com/userdata/userwork/15/468071015.mp3"}
 ];
 var song_now_rank=0;
+var video = $("#music_video")[0];
+var song_info = $("#song-info");
 
 console.log(songList[0].url);
 
@@ -12,6 +14,8 @@ $("#btn-next").bind("click touchstart",function(){
 	if(song_now_rank<songList.length-1){
 		song_now_rank++;
 		$("#music_video").attr("src",songList[song_now_rank].url);
+		$(song_info).text(songList[song_now_rank].name + '-' + songList[song_now_rank].singer);
+		video.play();
 	}
 });
 
@@ -19,6 +23,8 @@ $("#btn-pre").bind("click touchstart",function(){
 	if(song_now_rank>0){
 		song_now_rank--;
 		$("#music_video").attr("src",songList[song_now_rank].url);
+		$(song_info).text(songList[song_now_rank].name + '-' + songList[song_now_rank].singer);
+		video.play();
 	}
 });
 
